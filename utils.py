b71157769c89b983c55e858c9bfc8c062647fab4
Kirill <kirilldenisluka@gmail.com>
2025-01-25 13:07:32 -0500

    added preprocessing functions

--- utils.py
+++ utils.py
@@ -1,48 +1,55 @@
 import os
 import numpy as np
 from PIL import Image
-from tenserflow.keras.preprocessing.image import IamgeDataGenerator
+from tensorflow.keras.utils import to_categorical  # Correct import for to_categorical
 
-def load_data(image_dir, mask_dir):
+# Function to load data
+def load_data(image_dir, mask_dir, target_size=(128, 128), num_classes=2):
     images = []
     masks = []
 
-    # Load iamges
-    images_dir = os.path.join(data_dir, "images")
-    mask_dir = os_path.join(data_dir, "masks")
-
-    for img_name in os.lsitdir(image_dir):
+    # Load images and masks
+    for img_name in os.listdir(image_dir):  # Fixed typo in os.listdir
         img_path = os.path.join(image_dir, img_name)
         mask_path = os.path.join(mask_dir, img_name)
 
+        # Load and preprocess image
         img = Image.open(img_path).resize(target_size)
-        img = np.array(img) / 255.0
+        img = np.array(img) / 255.0  # Normalize image values to [0, 1]
 
-        mask = Image.open(mask_patch).resize(target_size)
+        # Load and preprocess mask
+        mask = Image.open(mask_path).resize(target_size)  # Fixed variable name typo
         mask = np.array(mask)
-        mask = to_categorical(mask, num_classes=num_classes)
+        mask = to_categorical(mask, num_classes=num_classes)  # One-hot encoding for segmentation
 
         images.append(img)
-        masks.apped(mask)
-    
-    return np.array(images)m np.array(masks)
+        masks.append(mask)
+
+    return np.array(images), np.array(masks)  # Fixed missing comma
 
 
-def data_generator(data_dir, batch_size=32, target_size=(128, 128)):
+# Generator function for loading data in batches
+def data_generator(image_dir, mask_dir, batch_size=32, target_size=(128, 128), num_classes=2):
     while True:
         images, masks = [], []
-        for _ in range(batch_size):
-            # Randomly sample
-            img_name = np.random.choices(os.listdir(os.path.join(data_dir, "images")))
-            img_path = os.path.join(data_dir, "images", img_name)
-            mask_path = os.path.join(data_dir, "masks", img_name)
+        img_names = os.listdir(image_dir)  # List of image names
 
-            img = Iamge.open(img_path).resize(target_size)
+        for _ in range(batch_size):
+            # Randomly sample an image and corresponding mask
+            img_name = np.random.choice(img_names)  # Fixed np.random.choices to np.random.choice
+            img_path = os.path.join(image_dir, img_name)
+            mask_path = os.path.join(mask_dir, img_name)
+
+            # Load and preprocess image
+            img = Image.open(img_path).resize(target_size)
             img = np.array(img) / 255.0
+
+            # Load and preprocess mask
             mask = Image.open(mask_path).resize(target_size)
             mask = np.array(mask)
-            mask = to_categorical(mask, num_classes=2)
+            mask = to_categorical(mask, num_classes=num_classes)
 
             images.append(img)
-            masks.append(mask)                                                                                                                                                                                                                                                                                                                                                                                                             Q                                                                   Q   122222222222222222222222        q12q@@@@@@@@@@@@@@@@@@@@@@@@@@q qAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA11@
-        yield np.array(images), np.array(masks)
+            masks.append(mask)
+
+        yield np.array(images), np.array(masks)  # Yield batch of images and masks
